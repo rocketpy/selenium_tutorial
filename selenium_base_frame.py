5ea8c6ddc00e054a5716c8d986571005cd461e5c
rocketpy <dimitr.ushakov@gmail.com>
2020-05-11 00:30:23 +0300

    added imports

--- selenium_base_frame.py
+++ selenium_base_frame.py
@@ -1,1 +1,19 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+
+
+path = "C:\Program Files\chromedriver.exe"
+
+driver = webdriver.Chrome(path)
+driver.get("https:// ... ")
+
+number = ""
+password = ""
+
 
