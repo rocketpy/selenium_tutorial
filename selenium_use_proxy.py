6c8796ec3d42185007c2c14ce3f8b405f692ce07
rocketpy <dimitr.ushakov@gmail.com>
2020-05-11 22:56:12 +0300

    updated

--- selenium_use_proxy.py
+++ selenium_use_proxy.py
@@ -3,9 +3,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 proxy = "12.345.678.910:8080"
-
 options = WebDriverWait.ChromeOptions()
 options.add_argument('--proxy-server=%s' % proxy)
-
 chrome = webdriver.Chrome(chrome_options=options)
 chrome.get("https://www ... ")
+
+
+#  or
+
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+
+prox = Proxy()
+prox.proxy_type = ProxyType.MANUAL
+prox.http_proxy = "ip_addr:port"
+prox.socks_proxy = "ip_addr:port"
+prox.ssl_proxy = "ip_addr:port"
+
+capabilities = webdriver.DesiredCapabilities.CHROME
+prox.add_to_capabilities(capabilities)
+
+driver = webdriver.Chrome(desired_capabilities=capabilities)
