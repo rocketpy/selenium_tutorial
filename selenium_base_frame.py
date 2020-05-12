19346ac843b7d4bf447d3b66e6fa358ea62d7090
rocketpy <dimitr.ushakov@gmail.com>
2020-05-12 23:57:22 +0300

    updated

--- selenium_base_frame.py
+++ selenium_base_frame.py
@@ -28,15 +28,18 @@ search = driver.find_element_by_id('text')  # input id_name
 time.sleep(1)  # use sleep , for simulation user actions
 search.send_keys('Kate Middleton')  #  she is so cute and beautiful ))
 time.sleep(1)
-search.send_keys(Keys.ENTER)
+search.send_keys(Keys.ENTER)  # ENTER , it's a button at keyboard
 time.sleep(1)
-driver.find_element_by_partial_link_text('Картинки').click()
+driver.find_element_by_partial_link_text('Картинки').click()  # or  by_link_text()
 
-tabs = driver.window_handles  # a list
-driver.switch_to.window(tabs[1])
-img1 = driver.find_elements_by_class_name('serp-item__link')  # list of elems
-img_link = img1[1].get_attribute('href')
-driver.get(img_link)
+#  working with opened tabs in browser
+tabs = driver.window_handles  # get a list of opened tabs 
+
+driver.switch_to.window(tabs[1])  # change window to second opened tab !!!
+
+imgs = driver.find_elements_by_class_name('serp-item__link')  # get a list of elements
+img_link = imgs[1].get_attribute('href')  # get href of img
+driver.get(img_link)  # go to this link
 
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, 'Открыть')))
