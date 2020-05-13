47289c39945c579d4f92c4b5e46c51bdaf2883df
rocketpy <dimitr.ushakov@gmail.com>
2020-05-13 22:14:24 +0300

    added main code

--- youtube_search.py
+++ youtube_search.py
@@ -1,1 +1,14 @@
+from selenium import webdriver
 
+
+path = "C:\Program Files\chromedriver.exe" 
+driver = webdriver.Chrome(path) 
+driver.get("https://youtube.com")
+
+search = driver.find_element_by_xpath("...")  # input field 
+search.send_keys("some_word")
+
+search_button = driver.find_element_by_xpath("...")  # button of search field
+search_button.click()
+
+driver.quit()
