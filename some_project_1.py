302be422d4833fea5c3493b1240fc100c6d32d5f
rocketpy <dimitr.ushakov@gmail.com>
2020-05-16 23:53:31 +0300

    updated

--- some_project_1.py
+++ some_project_1.py
@@ -1,5 +1,5 @@
 import os
-import time
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,11 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 PATH = "C:\Program Files\chromedriver.exe"  
-driver = webdriver.Chrome(PATH)   
+driver = webdriver.Chrome(PATH) 
+LOGIN_FB = ""
+PASSWORD_FB = ""
 
 
 def auth_fb():
     try:
-        driver.get('https://www.facebook.com')
+        driver.get("https://www.facebook.com")
+        sleep(5)
+        driver.find_element_by_id("email").send_keys(LOGIN_FB)
+        sleep(2)
+        driver.find_element_by_id("pass").send_keys(PASSWORD_FB)
+        sleep(2)
+        driver.find_element_by_id("u_0_b").click()
         
     
