72c0ef30e755221349b33a2d983ffbe4fbf0f01d
rocketpy <dimitr.ushakov@gmail.com>
2020-05-16 23:36:50 +0300

    added imports

--- some_project_2.py
+++ some_project_2.py
@@ -1,1 +1,13 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+path = "C:\..\chromedriver.exe" 
+
+driver = webdriver.Chrome(path)   # driver = webdriver.Firefox()
+driver.get("https:// ... ")
