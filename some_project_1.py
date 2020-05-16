7fdceb1c78e7ebf247e5d4a5c91a14b890f227bc
rocketpy <dimitr.ushakov@gmail.com>
2020-05-16 23:46:52 +0300

    added imports

--- some_project_1.py
+++ some_project_1.py
@@ -1,1 +1,18 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+PATH = "C:\Program Files\chromedriver.exe"  
+driver = webdriver.Chrome(PATH)   
+
+
+def auth_fb():
+    try:
+        driver.get('https://www.facebook.com')
+        
+    
