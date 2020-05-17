c574f7f8826ba58edf3963ecd6ccf8cda73f1c54
rocketpy <dimitr.ushakov@gmail.com>
2020-05-17 16:08:52 +0300

    added proxy

--- some_project_1.py
+++ some_project_1.py
@@ -1,16 +1,32 @@
 import os
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.support import expected_conditions as EC
 
 
 PATH = "C:\Program Files\chromedriver.exe"  
-driver = webdriver.Chrome(PATH) 
+PROXY = "12.345.678.910:8080"
+options = WebDriverWait.ChromeOptions()
+options.add_argument('--proxy-server=%s' % PROXY)
+chrome = webdriver.Chrome(chrome_options=options)
+#driver = webdriver.Chrome(PATH) 
 LOGIN_FB = ""
 PASSWORD_FB = ""
+
+"""
+proxy = Proxy()
+proxy.proxy_type = ProxyType.MANUAL
+prox.http_proxy = "ip_addr:port"
+prox.socks_proxy = "ip_addr:port"
+prox.ssl_proxy = "ip_addr:port"
+capabilities = webdriver.DesiredCapabilities.CHROME
+prox.add_to_capabilities(capabilities)
+driver = webdriver.Chrome(desired_capabilities=capabilities)
+"""
 
 
 def auth_fb():
