2cb2a0aff780f377eb2e638e638314e856a33654
rocketpy <dimitr.ushakov@gmail.com>
2020-05-18 22:41:06 +0300

    added imports

--- youtube_search.py
+++ youtube_search.py
@@ -1,4 +1,12 @@
+import os
+from time import sleep
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.support import expected_conditions as EC
 
 
 path = "C:\Program Files\chromedriver.exe" 
