425834b0a0769539fe37855a9d02998515aa0403
rocketpy <dimitr.ushakov@gmail.com>
2020-05-20 23:50:46 +0300

    added main code

--- selenium_unittest.py
+++ selenium_unittest.py
@@ -5,3 +5,21 @@ import unittest
 from selenium import webdriver
 
 
+# PATH = "C:\Program Files\chromedriver.exe"  
+
+class Search(unittest.TestCase):
+    
+    def set_up(self):
+        self.driver = webdriver.Chrome(PATH)
+        self.driver = .get("https"// ... ")
+
+    def test_case(self):
+        print("Some message")
+        assert True
+    
+    def tear_down():
+        self.driver.close()
+          
+                                               
+if __name__=="__maun__":
+    unittest.main()
