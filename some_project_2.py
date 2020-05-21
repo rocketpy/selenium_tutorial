d4d8cf040304a951e6b1810e2c287fa5d01a7165
rocketpy <dimitr.ushakov@gmail.com>
2020-05-21 23:15:38 +0300

    updated

--- some_project_2.py
+++ some_project_2.py
@@ -1,5 +1,7 @@
 import os
 import time
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,7 +9,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-path = "C:\..\chromedriver.exe" 
+#path = "C:\..\chromedriver.exe" 
+#driver = webdriver.Chrome(path)   # driver = webdriver.Firefox()
+#driver.get("https:// ... ")
 
-driver = webdriver.Chrome(path)   # driver = webdriver.Firefox()
-driver.get("https:// ... ")
+URL = "https://"
+with requests.Session() as se:
+    se.headers = {"User-Agent": "",
+                 "Accept-Language": "en"
+                 }
+    response = se.get(URL)
+soup = 
+
