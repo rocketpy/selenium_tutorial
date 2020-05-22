72e885a9774fe2e30c1df6e59a5fdab91449a7ba
rocketpy <dimitr.ushakov@gmail.com>
2020-05-22 23:38:51 +0300

    updated

--- selenium_change_UserAgent.py
+++ selenium_change_UserAgent.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+#  for Chrome
+opts = Options()
+user_agent = "any agent"
+opts.add_argument("user-agent=user_agent")
+
+driver = webdriver.Chrome(chrome_options=opts)
