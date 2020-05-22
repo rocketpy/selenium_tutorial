f13643ed993c58db4aef25efdd2b80aca2c5b35b
rocketpy <dimitr.ushakov@gmail.com>
2020-05-22 23:31:51 +0300

    added imports

--- some_project_rozetka.py
+++ some_project_rozetka.py
@@ -1,1 +1,20 @@
+import os
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.support import expected_conditions as EC
+
+
+#PATH = "C:\Program Files\chromedriver.exe" 
+#driver = webdriver.Chrome(PATH) 
+
+PROXY = "12.345.678.910:8080"
+options = WebDriverWait.ChromeOptions()
+options.add_argument('--proxy-server=%s' % PROXY)
+chrome = webdriver.Chrome(chrome_options=options)
+
 
