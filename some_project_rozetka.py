b5031d0b3b87be318f9adb3bf099a5db1b4e539b
rocketpy <dimitr.ushakov@gmail.com>
2020-05-27 15:37:20 +0300

    updated

--- some_project_rozetka.py
+++ some_project_rozetka.py
@@ -32,12 +32,17 @@ time.sleep(1)
 # time.sleep(1)
 driver.find_element_by_xpath('/html/body/app-root/div/div[1]/app-rz-header/header/div/div[2]/div[3]/form/button').click()
 
+# scroll page down
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+time.sleep(5)
+
 
 # get all notebooks
 driver.find_element_by_xpath('/html/body/app-root/div/div[1]/app-rz-main-page/div/main/main-page-content/app-fat-menu-tablet/nav/ul/li[1]/a').click()
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/app-root/div/div[1]/rz-super-portal/div/main/section/sp-dynamic-widgets/sp-widget-list[1]/section/ul/li[1]/sp-list-tile/div/a[2]').click()
 time.sleep(3)
+
 # here need use pagination !!!
 img_xpath = "/html/body/app-root/div/div[1]/rz-category/div/main/ctg-catalog/div/div[2]/section/div/ctg-grid/ul/li[1]/app-goods-tile-default/div/div[2]/a[1]/img[2]"
 name_xpath = "/html/body/app-root/div/div[1]/rz-category/div/main/ctg-catalog/div/div[2]/section/div/ctg-grid/ul/li[1]/app-goods-tile-default/div/div[2]/a[2]/span"
