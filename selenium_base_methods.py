9b7b94d9f304d455effe9763d0f886f0ce094a50
rocketpy <dimitr.ushakov@gmail.com>
2020-05-31 22:00:17 +0300

    added imports

--- selenium_base_methods.py
+++ selenium_base_methods.py
@@ -1,1 +1,6 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
