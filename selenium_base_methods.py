32ecde769b9912afafe768420e049f78fd10a7eb
rocketpy <dimitr.ushakov@gmail.com>
2020-05-31 22:14:41 +0300

    updated

--- selenium_base_methods.py
+++ selenium_base_methods.py
@@ -6,13 +6,29 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
 
+# use PROXY
+PROXY = "12.345.678.910:8080"
+options = WebDriverWait.ChromeOptions()
+options.add_argument('--proxy-server=%s' % PROXY)
+driver = webdriver.Chrome(chrome_options=options)  # Firefox()
+
+# without Proxy
 driver = webdriver.Chrome(path)  # or  webdriver.Chrome(executable_path=path)
 driver.get("https:// ... ")  # go to some URL
 
-driver.title  # title of a page
+# checking some INFO
+print(driver.title)  # title of a page
 print(driver.current_url)  # check a current url
 
-elem.clear()  # clean field before send a new text
+# for checking some element for exist
+print(elem.is_displayed())  # return True or False
+print(elem.is_enabled())  # use for check_box , return True or False
+
+# use INPUT field on web_page
+search = driver.find_element_by_id('text')  # input id_name 
+search.send_keys('some_word or text')  #  she is so cute and beautiful ))
+search.send_keys(Keys.ENTER)  # ENTER , it's a button at keyboard
+search.clear()  # clean field before send a new text
 
 
 # to close window
