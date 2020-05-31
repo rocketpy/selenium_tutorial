df003f80fac5d04c58ba91344323a13c3aba66dd
rocketpy <dimitr.ushakov@gmail.com>
2020-05-31 22:07:59 +0300

    updated

--- selenium_base_methods.py
+++ selenium_base_methods.py
@@ -4,3 +4,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome(path)  # or  webdriver.Chrome(executable_path=path)
+driver.get("https:// ... ")  # go to some URL
+
+driver.title  # title of a page
+print(driver.current_url)  # check a current url
+
+elem.clear()  # clean field before send a new text
+
+
+# to close window
+driver.quit()  
+# or 
+driver.close()
