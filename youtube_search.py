1c57eb79c5400bf3b1b5980422bd0c0a07e1d19f
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-06-20 18:52:07 +0300

    modified

--- youtube_search.py
+++ youtube_search.py
@@ -7,20 +7,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
-PATH = "C:\Program Files\chromedriver.exe"  
+# PATH = r"C:\...\chromedriver.exe"  
 PROXY = "12.345.678.910:8080"
-options = WebDriverWait.ChromeOptions()
+# driver = webdriver.Chrome(PATH)
+options = webdriver.ChromeOptions()
 options.add_argument('--proxy-server=%s' % PROXY)
-chrome = webdriver.Chrome(chrome_options=options)  # Firefox()
-#driver = webdriver.Chrome(PATH) 
+driver = webdriver.Chrome(chrome_options=options)  # Firefox()
+
 driver.get("https://youtube.com")
 
-search = driver.find_element_by_xpath("//*[@id="search"]")  # input field 
-search.send_keys("some_word")
+search = driver.find_element_by_xpath('//*[@id="search"]')  # input field 
+search.send_keys("Prodigy")
 
-search_button = driver.find_element_by_xpath("//*[@id="search-icon-legacy"]")  # button of search field
+search_button = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')  # button of search field
+
 search_button.click()
 
 #driver.quit()
