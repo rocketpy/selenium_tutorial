d830f49f8bf742b7aae01bad2b95de4b80007595
rocketpy <dimitr.ushakov@gmail.com>
2020-06-24 22:43:47 +0300

    updated

--- examples.py
+++ examples.py
@@ -1,27 +1,24 @@
-# need add function for change Proxy and UserAgent !!!
-from selenium import webdriver
 import time
 import random
+from selenium import webdriver
 from fake_useragent import UserAgent
 from datetime import datetime as dt
+# https://pypi.org/project/fake-useragent/
 
+
+# need add function for change Proxy and UserAgent !!!
 
 def add_random_ua():
-    # Generate a random user-agent useing fake_useragent library
     fake_ua = UserAgent().random
-    # If there is already a "user-agent" argument in options delete it
     for item in options.arguments:
         if '--user-agent=' in item:
             options.arguments.remove(item)
-    # Add generated user-agent to options
     options.add_argument(f'--user-agent={fake_ua}')
     print(f'User-agent: {fake_ua}')
 
 
 def add_random_proxy():
-    # Same logic as with add_random_ua
-    # PROXY_LIST is just a list read from .txt file
-    random_proxy = PROXY_LIST[random.randint(0, len(PROXY_LIST) - 1)]
+    random_proxy = PROXY_LIST[random.randint(0, len(PROXY_LIST) - 1)]  # PROXY_LIST taking from .txt file
     for item in options.arguments:
         if '--proxy-server=' in item:
             options.arguments.remove(item)
@@ -29,7 +26,7 @@ def add_random_proxy():
     print(f'Proxy-server: {random_proxy}')
 
 chromedriver = 'C:\\Users\\User\\Desktop\\chromedriver\\chromedriver.exe'
-# Initial chromedriver options
+
 options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1024,768")
 options.add_argument("--disable-notifications")
@@ -38,17 +35,14 @@ options.add_argument("--user-data-dir=C:\\Users\\User\\AppData\\Local\\Google\\C
 options.add_argument("--profile-directory=Default")
 options.add_argument("--ignore-certificate-errors")
 
-while True:
-    print(f'Timestamp: {dt.now().strftime("%Y-%m-%d %H:%M:%S")}')
+while True:  # 
+    # print(f'Timestamp: {dt.now().strftime("%Y-%m-%d %H:%M:%S")}')
     add_random_ua()
     add_random_proxy()
     web = webdriver.Chrome(chromedriver, options=options)
     web.implicitly_wait(30)
     web.get(URL)
 
-    # Some testing code...
+    #  testing code here ...
 
     web.quit()
-
-
-
