6d943a62acf1c7a368f9512eb91c8b086b27ed04
rocketpy <dimitr.ushakov@gmail.com>
2020-07-08 23:54:20 +0300

    updated

--- selenium_links.py
+++ selenium_links.py
@@ -1,1 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+href = driver.find_element_by_link_text("Recommend Selenium").get_attribute("href")
+print(href) # recomand.html
+ 
+id = driver.find_element_by_link_text("Recommend Selenium").get_attribute("id")
+print(id) #  recommend_selenium_link
+ 
+text = driver.find_element_by_id("recommend_selenium_link").text
+print(text) # Recommend Selenium
+ 
+tag_name = driver.find_element_by_id("recommend_selenium_link").tag_name
+print(tag_name) #  a
