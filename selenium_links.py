aaeb9d9ab97624e72554162480e3514b787504d0
rocketpy <dimitr.ushakov@gmail.com>
2020-07-09 00:41:56 +0300

    updated

--- selenium_links.py
+++ selenium_links.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 #  read the docs : https://selenium-python.readthedocs.io/
 
+# find_element_by_link_text  return first link !!!
 
 #  get attribute href
 href = driver.find_element_by_link_text("some text").get_attribute("href")
@@ -25,11 +26,21 @@ print(tag_name) #  a
 # by xpath
 driver.find_element_by_xpath("//p/a[text()='some text']").click()
 
-# click on second href with same name
-driver.find_elements_by_link_text("Some second same link")[1].click()
+# find link by particial text
+driver.find_element_by_partial_link_text("partial").click()
 
-# find_element_by_link_text  return first link !!!
+# click on second href with same name
+driver.find_elements_by_link_text("Secod link with same name")[1].click()
 
 #  click on second link with same name
 xpath = '//div[contains(text(), "Second")]/a[text()="Click here"]'
 driver.find_element_by_xpath(xpath).click()
+
+# when href open new window
+current_url = driver.current_url
+
+new_window_url = driver.find_element_by_link_text("Open new window").get_attribute("href")
+driver.get(new_window_url)
+ 
+driver.find_element_by_name("name").send_keys("sometext")
+driver.get(current_url) # back
