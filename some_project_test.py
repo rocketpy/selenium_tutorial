cbbb4613e3c898fa5402ebfbedd1266d8d113253
rocketpy <dimitr.ushakov@gmail.com>
2020-07-10 23:55:54 +0300

    updated

--- some_project_test.py
+++ some_project_test.py
@@ -1,1 +1,10 @@
-
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+ 
+  
+opts = Options()
+opts.set_headless()
+assert opts.headless  # without grafic interface !!!
+ 
+browser = Firefox(options=opts)
+browser.get('https://duckduckgo.com')
