637ed81778642caaec452bd2d296dc3d072fbba5
rocketpy <dimitr.ushakov@gmail.com>
2020-07-10 23:53:08 +0300

    updated

--- login_to_facebook.py
+++ login_to_facebook.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 user_name = "YOUR EMAILID"
@@ -15,5 +16,18 @@ element.send_keys(user_name)
 element = driver.find_element_by_id("pass")
 element.send_keys(password)
 element.send_keys(Keys.RETURN)
+
+
+username = browser.find_element_by_id("email")
+password = browser.find_element_by_id("pass")
+submit   = browser.find_element_by_id("loginbutton")
+
+username.send_keys("YOUR EMAILID")
+password.send_keys("YOUR PASSWORD")
+
+submit.click()
+wait = WebDriverWait( browser, 5 )
+page_title = browser.title
+assert page_title == "Facebook"
 
 # element.close()
