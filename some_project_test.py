e04bf3ac9d96aacbf66a2bf0b3a9cdb8abd06e92
rocketpy <dimitr.ushakov@gmail.com>
2020-07-11 23:41:21 +0300

    updated

--- some_project_test.py
+++ some_project_test.py
@@ -12,3 +12,9 @@ browser.get('https://duckduckgo.com')
 search_form = browser.find_element_by_id('search_form_input_homepage')
 search_form.send_keys('some key')
 search_form.submit()
+
+results = browser.find_elements_by_class_name('result')
+# print(results[0].text)
+
+browser.close()
+quit()
