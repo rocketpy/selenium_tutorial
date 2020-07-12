d6687f71daadf17bda06db8f05ed7cf4a9bfe553
rocketpy <dimitr.ushakov@gmail.com>
2020-07-12 23:45:52 +0300

    updated

--- some_project_test.py
+++ some_project_test.py
@@ -36,4 +36,20 @@ tracks = browser.find_elements_by_class_name('discover-item')
 len(tracks)  #  quantity a tracks
 tracks[3].click()  # play track number 4
 
+# using pagination , for next 8 tracks
+next_button = [i for i in browser.find_elements_by_class_name('item-page')
+                   if e.text.lower().find('next') > -1]
+next_button.click()
 
+# tracks = browser.find_elements_by_class_name('discover-item')
+# assert(len(tracks) == 8) 
+
+section = self.browser.find_element_by_class_name('discover-results')
+left_x = section.location['x']
+right_x = left_x + section.size['width']
+items = browser.find_element_by_class_name('discover_items')
+ 
+tracks = [t for t in items
+              if t.location['x'] >= left_x and t.location['x'] < right_x]
+ 
+assert len(tracks) == 8
