fbdbb4733e7327fb0754a9d701f150068093d5fd
rocketpy <dimitr.ushakov@gmail.com>
2020-07-13 23:55:08 +0300

    added imports

--- example_authomate.py
+++ example_authomate.py
@@ -1,1 +1,10 @@
+import csv
+from os.path import isfile
+from time import sleep, ctime
+from collections import namedtuple
+from threading import Thread
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+
+
 
