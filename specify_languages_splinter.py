139f50eb7cc7f9754f1e3cc710f354276e849b1c
rocketpy <dimitr.ushakov@gmail.com>
2020-07-24 23:37:42 +0300

    updated

--- specify_languages_splinter.py
+++ specify_languages_splinter.py
@@ -4,7 +4,7 @@ from splinter.driver.webdriver import BaseWebDriver, WebDriverElement
 
 
 options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'de_DE'})
+options.add_experimental_option('prefs', {'intl.accept_languages': 'ua_UA'})
 
 browser = BaseWebDriver()
 browser.driver = Chrome(chrome_options=options)
@@ -12,3 +12,14 @@ browser.driver = Chrome(chrome_options=options)
 browser.visit('https://www...')
 
 
+#  splinter API only
+from splinter import Browser
+from selenium import webdriver
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('prefs', {'intl.accept_languages': 'ua_UA'})
+
+browser = Browser('chrome', options=options)
+
+browser.visit('http://www...')
