bc55d7c9d0877e7affb614a0e7a912103c38755b
rocketpy <dimitr.ushakov@gmail.com>
2020-07-24 23:35:52 +0300

    updated

--- specify_languages_splinter.py
+++ specify_languages_splinter.py
@@ -1,1 +1,14 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from splinter.driver.webdriver import BaseWebDriver, WebDriverElement
+
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': 'de_DE'})
+
+browser = BaseWebDriver()
+browser.driver = Chrome(chrome_options=options)
+
+browser.visit('https://www...')
+
 
