eb006745b0de6a820e6209092924bd64caa06cb9
rocketpy <dimitr.ushakov@gmail.com>
2020-08-05 16:12:42 +0300

    updated

--- if_not_exist_id.py
+++ if_not_exist_id.py
@@ -1,1 +1,16 @@
-
+def check_exists_by_id(id):
+    try:
+        driver.find_element_by_id(id).click()
+        except NoSuchElementException:
+        return False
+        return True
+        # print(check_exists_by_xpath(home))
+def find_by_name():
+    if (check_exists_by_id(id)) is True:
+        driver.find_element_by_id(id).click()
+        # print(‘xpath - pass’)
+    else:
+        driver.find_element_by_class_name(name).click()
+        # print(‘xpath - fail > class - pass’)
+        
+    find_by_name()
