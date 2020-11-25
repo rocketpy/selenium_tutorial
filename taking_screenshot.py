a21cd94c6458c995f1748d321b7ac499e07cd101
rocketpy <dimitr.ushakov@gmail.com>
2020-11-25 21:58:22 +0200

    updated

--- taking_screenshot.py
+++ taking_screenshot.py
@@ -1,1 +1,8 @@
+from selenium import webdriver
 
+
+PATH = 'C:\\Users\\User\\Desktop\\chromedriver\\chromedriver.exe'
+
+driver = webdriver.Chrome(PATH)
+driver.maximize_window()
+driver.get("https://")
