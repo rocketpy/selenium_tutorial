64ba5b34f88ee80b6528d06ed6edb571a67985f9
rocketpy <dimitr.ushakov@gmail.com>
2020-11-25 22:10:46 +0200

    updated

--- taking_screenshot.py
+++ taking_screenshot.py
@@ -1,8 +1,16 @@
+import random
 from selenium import webdriver
 
 
-PATH = 'C:\\Users\\User\\Desktop\\chromedriver\\chromedriver.exe'
+PATH = "C:\\chromedriver.exe" 
+# FILE_NAME = "first"
+# or
+FILE_NAME = random.randint(1, 1000000)
 
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(PATH) 
 driver.maximize_window()
 driver.get("https://")
+driver.get_screenshot_as_file(FILE_NAME + ".png")  # .jpg
+
+driver.quit()
+# driver.close()
