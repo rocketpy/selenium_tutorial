fd781ac60112a3f5cdf1abdc3004e8bb92264ba0
rocketpy <dimitr.ushakov@gmail.com>
2020-12-12 17:07:54 +0200

    updated

--- cookies_save_load.py
+++ cookies_save_load.py
@@ -1,4 +1,22 @@
+#  Using JSON
+import json
+
+
+def save_cookie(driver, path):
+    with open(path, 'w') as filehandler:
+        json.dump(driver.get_cookies(), filehandler)
+
+def load_cookie(driver, path):
+    with open(path, 'r') as cookiesfile:
+        cookies = json.load(cookiesfile)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+        
 #  Using pickle
+import pickle
+
+
 def save_cookie(driver, path):
     with open(path, 'wb') as filehandler:
         pickle.dump(driver.get_cookies(), filehandler)
