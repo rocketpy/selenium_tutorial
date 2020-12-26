ea30dc00539370b98d3b839bc24432c9864ab87b
rocketpy <dimitr.ushakov@gmail.com>
2020-12-26 22:15:46 +0200

    Update selenium_wire.py

--- selenium_wire.py
+++ selenium_wire.py
@@ -1,1 +1,20 @@
+#  Extends Selenium to give you the ability to inspect requests made by the browser.
 
+#  PyPi:  https://pypi.org/project/selenium-wire/
+
+#  pip install selenium-wire
+
+#  example
+from seleniumwire import webdriver 
+
+
+driver = webdriver.Firefox()
+
+driver.get('https://www.google.com')
+
+for request in driver.requests:
+    if request.response:
+        print(request.url,
+              request.response.status_code,
+              request.response.headers['Content-Type']
+              )
