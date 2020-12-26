11fb55b6e7e3538c8bce118d6693709408cf7563
rocketpy <dimitr.ushakov@gmail.com>
2020-12-26 22:05:48 +0200

    Update Firefox_frame.py

--- Firefox_frame.py
+++ Firefox_frame.py
@@ -1,1 +1,10 @@
+import time
+from selenium import webdriver
+from seleniumwire import webdriver
+from fake_useragent import UserAgent
 
+
+URL = "https://www.../"
+
+# options
+options = webdriver.FirefoxOptions()
