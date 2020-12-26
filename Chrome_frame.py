d716aa576bc546f4087fcb7ca4e506e09d01ae8e
rocketpy <dimitr.ushakov@gmail.com>
2020-12-26 22:17:32 +0200

    Update Chrome_frame.py

--- Chrome_frame.py
+++ Chrome_frame.py
@@ -1,1 +1,10 @@
+import time
+import random
+# from selenium import webdriver
+from seleniumwire import webdriver
+from fake_useragent import UserAgent
 
+
+URL = "https://www...com/"
+
+user_agents_list = []
