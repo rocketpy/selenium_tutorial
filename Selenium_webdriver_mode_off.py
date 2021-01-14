c08c708886faed53177b9bec683a7503dbdd6876
rocketpy <dimitr.ushakov@gmail.com>
2021-01-14 23:06:30 +0200

    Update Selenium_webdriver_mode_off.py

--- Selenium_webdriver_mode_off.py
+++ Selenium_webdriver_mode_off.py
@@ -21,3 +21,12 @@ driver = webdriver.Chrome(executable_path="...",
                           options=options
                          )
 
+try:
+    driver.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")  # checking , working or not a mode_off !
+    # time.sleep(5)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
+ 
