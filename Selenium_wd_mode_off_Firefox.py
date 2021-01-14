477a2578f547ea49a5b9c241ed5d1c0352575cbd
rocketpy <dimitr.ushakov@gmail.com>
2021-01-14 23:14:54 +0200

    Update Selenium_wd_mode_off_Firefox.py

--- Selenium_wd_mode_off_Firefox.py
+++ Selenium_wd_mode_off_Firefox.py
@@ -2,6 +2,10 @@ import time
 from selenium import webdriver
 
 
+# "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+# r"C:\users\selenium_python\chromedriver\chromedriver.exe"
+
+
 options = webdriver.FirefoxOptions()
 options.set_preference("general.useragent.override", "user-agent")
 
@@ -10,3 +14,13 @@ options.set_preference("dom.webdriver.enabled", False)
 driver = webdriver.Firefox(executable_path="...",
                            options=options
                           )
+try:
+    driver.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")  # checking , mode off or not !
+    time.sleep(5)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
+
+    
