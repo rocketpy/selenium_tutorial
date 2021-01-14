838a1334d46c34af08a7a59f19843dc05221b1ee
rocketpy <dimitr.ushakov@gmail.com>
2021-01-14 23:11:08 +0200

    Update Selenium_wd_mode_off_Firefox.py

--- Selenium_wd_mode_off_Firefox.py
+++ Selenium_wd_mode_off_Firefox.py
@@ -1,1 +1,12 @@
+import time
+from selenium import webdriver
 
+
+options = webdriver.FirefoxOptions()
+options.set_preference("general.useragent.override", "user-agent")
+
+options.set_preference("dom.webdriver.enabled", False)
+
+driver = webdriver.Firefox(executable_path="...",
+                           options=options
+                          )
