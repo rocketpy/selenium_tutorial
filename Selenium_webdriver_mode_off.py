830ed2c8c6488de6e7191a8277d45788d52bfe95
rocketpy <dimitr.ushakov@gmail.com>
2021-01-14 23:05:27 +0200

    Update Selenium_webdriver_mode_off.py

--- Selenium_webdriver_mode_off.py
+++ Selenium_webdriver_mode_off.py
@@ -1,1 +1,23 @@
+import time
+from selenium import webdriver
+
+
+# "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+# r"C:\users\selenium_python\chromedriver\chromedriver.exe"
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=...")
+
+#  webdriver mode OFF
+
+#  for ChromeDriver under version 79.0.3945.16
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option("useAutomationExtension", False)
+
+# for ChromeDriver version 79.0.3945.16 or higher
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+driver = webdriver.Chrome(executable_path="...",
+                          options=options
+                         )
 
