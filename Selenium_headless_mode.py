82d73aa8d0cbe3d60161ec4550a42f3ad27697fd
rocketpy <dimitr.ushakov@gmail.com>
2021-01-17 21:57:53 +0200

    Update Selenium_headless_mode.py

--- Selenium_headless_mode.py
+++ Selenium_headless_mode.py
@@ -1,1 +1,10 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=...")
+
+# for ChromeDriver version 79.0.3945.16 or over
+options.add_argument("--disable-blink-features=AutomationControlled")
