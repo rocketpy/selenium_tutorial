3b9d0b71540dd94b80b24b6771819f1f870e5a7c
rocketpy <dimitr.ushakov@gmail.com>
2021-01-17 22:04:18 +0200

    Update Selenium_headless_mode.py

--- Selenium_headless_mode.py
+++ Selenium_headless_mode.py
@@ -7,4 +7,22 @@ options = webdriver.ChromeOptions()
 options.add_argument("user-agent=...")
 
 # for ChromeDriver version 79.0.3945.16 or over
-options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_argument("--disable-blink-features=AutomationControlled")  #  using webdriver mode off
+
+# headless mode's
+options.add_argument("--headless")  # best practice use this one
+#  OR
+options.headless = True
+
+driver = webdriver.Chrome(executable_path="PycharmProjects/selenium_python/chromedriver/chromedriver",
+                          options=options
+                         )
+try:
+    driver.get("https://www...")
+    time.sleep(5)
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
