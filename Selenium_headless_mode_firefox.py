9fa77e02c845e43d328f1ff65d565b530e120139
rocketpy <dimitr.ushakov@gmail.com>
2021-01-18 21:00:57 +0200

    Update Selenium_headless_mode_firefox.py

--- Selenium_headless_mode_firefox.py
+++ Selenium_headless_mode_firefox.py
@@ -7,3 +7,19 @@ options = webdriver.FirefoxOptions()
 options.set_preference("general.useragent.override", "user_agent_name")
 
 options.set_preference("dom.webdriver.enabled", False)  # disable webdriver mode
+
+# using headless mode 
+options.headless = True
+
+driver = webdriver.Firefox(executable_path="PycharmProjects/selenium_python/firefoxdriver/geckodriver",
+                           options=options)
+
+try:
+    driver.get("https://www...")
+    time.sleep(3)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()    
+ 
