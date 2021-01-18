d1e3f0e398e20082bcd9a2f4fee5b0639854e849
rocketpy <dimitr.ushakov@gmail.com>
2021-01-18 20:58:03 +0200

    Update Selenium_headless_mode_firefox.py

--- Selenium_headless_mode_firefox.py
+++ Selenium_headless_mode_firefox.py
@@ -1,1 +1,9 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+
+options = webdriver.FirefoxOptions()
+options.set_preference("general.useragent.override", "user_agent_name")
+
+options.set_preference("dom.webdriver.enabled", False)  # disable webdriver mode
