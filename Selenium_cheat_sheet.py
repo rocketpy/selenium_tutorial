18ca52a18878b0e13a16e4d0734332615f38679b
rocketpy <dimitr.ushakov@gmail.com>
2021-01-18 21:06:03 +0200

    Update Selenium_cheat_sheet.py

--- Selenium_cheat_sheet.py
+++ Selenium_cheat_sheet.py
@@ -1,1 +1,16 @@
+# import selenium
+from selenium import webdriver
+
+
+# setup driver's
+firefoxdriver = webdriver.Firefox(executable_path="Path to Firefox driver")
+
+chromedriver = webdriver.Chrome(executable_path="Path to Chrome driver")
+
+iedriver = webdriver.IE(executable_path="Path to IEDriverServer.exe")
+
+edgedriver = webdriver.Edge(executable_path="Path to MicrosoftWebDriver.exe")
+
+operadriver = webdriver.Opera(executable_path="Path to operadriver")
+
 
