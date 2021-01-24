e655328c3bf0a80086a466d1fef59081532480c8
rocketpy <dimitr.ushakov@gmail.com>
2021-01-24 18:54:10 +0200

    Update Selenium_tabs_chrome.py

--- Selenium_tabs_chrome.py
+++ Selenium_tabs_chrome.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 
 
@@ -8,4 +7,23 @@ options.add_argument("user-agent=...")
 driver = webdriver.Chrome(executable_path="/PycharmProjects/selenium_python/chromedriver/chromedriver", 
                           options=options)
 
+try:
+    driver.get("https://www...")
+    # driver.implicitly_wait(5)
+    # print(driver.window_handles)  # driver.window_handles - return a list with opened tabs
+    # print(f"Currently URL is: {driver.current_url}")  # checking a current URL
+    
+    driver.switch_to.window(driver.window_handles[1])  # go to second tab
+    print(f"Currently URL is: {driver.current_url}")  # checking a current URL
+    
+    driver.close()
+    
+    driver.switch_to.window(driver.window_handles[0])  # go back to first opened tab
+    print(f"Currently URL is: {driver.current_url}")
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
 
