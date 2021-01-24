adf2c7b1a69fa7d1b280be56d58823226551a753
rocketpy <dimitr.ushakov@gmail.com>
2021-01-24 18:38:43 +0200

    Update Selenium_tabs_chrome.py

--- Selenium_tabs_chrome.py
+++ Selenium_tabs_chrome.py
@@ -1,1 +1,11 @@
+import time
+from selenium import webdriver
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=...")
+
+driver = webdriver.Chrome(executable_path="/PycharmProjects/selenium_python/chromedriver/chromedriver", 
+                          options=options)
+
 
