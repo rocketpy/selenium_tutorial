86456b580b5100f9c02bea00361bd1ae692617b6
rocketpy <dimitr.ushakov@gmail.com>
2021-01-28 23:16:16 +0200

    Update Selenium_snippets.py

--- Selenium_snippets.py
+++ Selenium_snippets.py
@@ -15,3 +15,19 @@ class VisualLayoutTest(BaseCase):
         # Click a button that makes a hidden element visible
         self.click("button")
         self.check_window(name="helloworld", level=3)
+
+        
+#  here's an example where a button is removed from a web page
+
+from seleniumbase import BaseCase
+
+
+class VisualLayoutTest(BaseCase):
+
+    def test_python_home_layout_change_failure(self):
+        self.open('https://www...')
+        self.check_window(name="python_home", baseline=True)
+        self.remove_element('a.button')
+        self.check_window(name="python_home", level=3)        
+        
+       
