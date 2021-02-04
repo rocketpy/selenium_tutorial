6eb05e8e6191ab870cfc45ce8237ab4a22f871fe
rocketpy <dimitr.ushakov@gmail.com>
2021-02-04 20:28:59 +0200

    Update Selenium_Helium.py

--- Selenium_Helium.py
+++ Selenium_Helium.py
@@ -1,1 +1,20 @@
+#  Lighter browser automation based on Selenium.
+
+#  Docs:  https://github.com/mherrmann/selenium-python-helium
+
+#  pip install helium
+
+"""
+python3 -m venv venv
+This creates a virtual environment in the venv directory. To activate it:
+
+# On Mac/Linux:
+source venv/bin/activate
+# On Windows:
+call venv\scripts\activate.bat
+Then, install Helium using pip:
+
+python -m pip install helium
+Now enter python into the command prompt and (for instance) the commands in the animation at the top of this page (from helium import *, ...).
+"""
 
