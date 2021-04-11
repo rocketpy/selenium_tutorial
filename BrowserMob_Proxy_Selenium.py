47dbbdffe14edcbde79634eb366fb8758a661bd4
rocketpy <dimitr.ushakov@gmail.com>
2021-04-11 18:05:12 +0300

    Update BrowserMob_Proxy_Selenium.py

--- BrowserMob_Proxy_Selenium.py
+++ BrowserMob_Proxy_Selenium.py
@@ -39,3 +39,36 @@ server.start()
 time.sleep(1)
 proxy = server.create_proxy()
 time.sleep(1)
+
+"""
+import time
+import psutil
+from browsermobproxy import Server
+
+
+for proc in psutil.process_iter():
+    # check whether the process name matches
+    if proc.name() == "browsermob-proxy":
+        proc.kill()
+
+dict = {'port': 8090}
+server = Server(path="./BrowserMobProxy/bin/browsermob-proxy", options=dict)
+server.start()
+time.sleep(1)
+proxy = server.create_proxy()
+time.sleep(1)
+from selenium import webdriver
+profile = webdriver.FirefoxProfile()
+selenium_proxy = proxy.selenium_proxy()
+profile.set_proxy(selenium_proxy)
+driver = webdriver.Firefox(firefox_profile=profile)
+
+
+proxy.new_har("file_name")
+driver.get("http://www...")
+print (proxy.har) # returns a HAR JSON blob
+
+server.stop()
+driver.quit()
+"""
+
