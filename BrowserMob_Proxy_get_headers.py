16768ee016bc01296e9893973e91e7ab6e7a29e6
rocketpy <dimitr.ushakov@gmail.com>
2021-05-04 23:07:46 +0300

    Update BrowserMob_Proxy_get_headers.py

--- BrowserMob_Proxy_get_headers.py
+++ BrowserMob_Proxy_get_headers.py
@@ -1,1 +1,37 @@
+import settings
+from selenium import webdriver
+from browsermobproxy import Server
+from selenium.webdriver import DesiredCapabilities
+
+
+config = settings.Config
+
+server = Server(config.BROWSERMOB_PATH)
+server.start()
+proxy = server.create_proxy()
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server=%s' % proxy.proxy)
+chrome_options.add_argument('--headless')
+
+capabilities = DesiredCapabilities.CHROME.copy()
+capabilities['acceptSslCerts'] = True
+capabilities['acceptInsecureCerts'] = True
+
+driver = webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities,
+                          executable_path=config.CHROME_PATH)
+
+proxy.new_har("sahibinden", options={'captureHeaders': True})
+driver.get("https://www...")
+
+entries = proxy.har['log']["entries"]
+for entry in entries:
+    if 'request' in entry.keys():
+        print(entry['request']['url'])
+        print(entry['request']['headers'])
+        print('\n')
+
+proxy.close()
+driver.quit()
 
