a81d38b20bcd32ef1e6154623bd07b0da9b380e7
rocketpy <dimitr.ushakov@gmail.com>
2021-05-16 23:27:55 +0300

    Update Selenium_stealth_mode.py

--- Selenium_stealth_mode.py
+++ Selenium_stealth_mode.py
@@ -1,1 +1,27 @@
+#  Selenium-stealth - Trying to make python selenium more stealthy.
 
+# PyPi:  https://pypi.org/project/selenium-stealth/
+# Github: https://github.com/diprajpatra/selenium-stealth
+
+# pip install selenium-stealth
+
+# Important:  As of now selenium-stealth only support Selenium Chrome !!!
+
+#  IMPORTANT
+"""
+A python package selenium-stealth to prevent detection. This programme is trying to make python selenium more stealthy.
+
+As of now selenium-stealth only support Selenium Chrome.
+
+After using selenium-stealth you can prevent almost all selenium detections.
+There is a lot of guides on stackoverflow on How to prevent selenium detection but I can not find a single python package
+for it so I am just creating one after all we can't let the cats win.
+It can be seen as a re-implementation of JavaScript puppeteer-extra-plugin-stealth developed by @berstend.
+
+Features that currently selenium-stealth can offer:
+✅️ selenium-stealth with stealth passes all public bot tests.
+
+✅️ With selenium-stealth selenium can do google account login.
+
+✅️ selenium-stealth help with maintaining a normal reCAPTCHA v3 score
+"""
