98f42690b4ba3e937239f47dcb8c381382039b77
rocketpy <dimitr.ushakov@gmail.com>
2021-06-09 23:17:24 +0300

    Update Selenium_Firefox.py

--- Selenium_Firefox.py
+++ Selenium_Firefox.py
@@ -1,1 +1,16 @@
+#  selenium_firefox
+"""
+Features:
+-Easily create a firefox selenium webdriver with proxy(host/port),
+ extensions and other settings, such as, full-screen-window, private session.
+-Override user-agent
+-Easily save and load cookies for websites
+"""
+
+#  PyPi: https://pypi.org/project/selenium-firefox/
+#  Github: https://github.com/kkristof200/selenium_firefox
+
+#  pip3 install selenium-firefox
+
+# Dependencies: geckodriver-autoinstaller, noraise, selenium, tldextract
 
