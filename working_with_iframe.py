9cf8062f6cc0269805c1ea49e5f07e602bf44e65
rocketpy <dimitr.ushakov@gmail.com>
2021-07-17 23:15:04 +0300

    Update working_with_iframe.py

--- working_with_iframe.py
+++ working_with_iframe.py
@@ -1,1 +1,22 @@
+import time
+import html5lib
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+
+driver.get('http://...')
+try:
+    time.sleep(4)
+    iframe = driver.find_elements_by_tag_name('iframe')[0]
+    driver.switch_to_default_content()
+    driver.switch_to_frame(iframe)
+    driver.switch_to_default_content()
+    driver.find_elements_by_tag_name('iframe')[0]
+    output = driver.page_source
+    print(output)
+
+finally:
+    driver.quit()
