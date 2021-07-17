edd008bd83fee63b947885ca6cec68d9bc66a197
rocketpy <dimitr.ushakov@gmail.com>
2021-07-17 23:18:04 +0300

    Update working_with_iframe.py

--- working_with_iframe.py
+++ working_with_iframe.py
@@ -20,3 +20,20 @@ try:
 
 finally:
     driver.quit()
+    
+    
+# How to switch iframe inside iframe !!!
+
+# switch_to_default_content () will take you back to the beginning of the document.
+# What was happening is that you switched to the first iframe, switched to the top of the document again,
+# and then tried to find the second iframe. Selenium cannot find the second iframe because it is inside the first iframe.
+
+# If you remove the second switch_to_default_content () 
+"""
+iframe = driver.find_elements_by_tag_name('iframe')[0]
+driver.switch_to.default_content()
+
+driver.switch_to.frame(iframe)
+driver.find_elements_by_tag_name('iframe')[0]
+
+"""
