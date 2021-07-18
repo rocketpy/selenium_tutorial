8fc9c16b627cb772588492d0b574206f5a517482
rocketpy <dimitr.ushakov@gmail.com>
2021-07-18 23:04:56 +0300

    Update Mouse_Actions.py

--- Mouse_Actions.py
+++ Mouse_Actions.py
@@ -1,1 +1,17 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
+
+class Test(unittest.TestCase):
+    def test_move_to_element(self):
+        driver = webdriver.Chrome(executable_path=r'/chromedriver.exe')
+        driver.implicitly_wait(30)
+        driver.get("https://www...")
+        menu = driver.find_element_by_id("sub-menu")
+        actions = ActionChains(driver)
+        actions.move_to_element(menu)
+        actions.perform()
+
+if __name__ == "__main__":
+    unittest.main()
