7653b36342c8961642a80c48d48d5e154924dd46
rocketpy <dimitr.ushakov@gmail.com>
2021-07-24 23:55:52 +0300

    Update Selenium_cheat_sheet.py

--- Selenium_cheat_sheet.py
+++ Selenium_cheat_sheet.py
@@ -81,3 +81,17 @@ click.click()
 driver.quit()  
 # or driver.close()
 
+
+# using Try, Finally
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+try:
+    element = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, "id-of-new-element"))
+    )
+finally:
+    driver.quit()
+
