ee320361cf8caaf5e52d65e7926734bf8b091166
rocketpy <dimitr.ushakov@gmail.com>
2021-08-11 23:33:35 +0300

    Update drag_and_drop.py

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -1,1 +1,11 @@
+# For use drag and drop, either moving an element by a certain amount, or on to another element:
+from selenium import webdriver
 
+
+element = driver.find_element_by_name("source")
+target = driver.find_element_by_name("target")
+
+from selenium.webdriver import ActionChains
+action_chains = ActionChains(driver)
+action_chains.drag_and_drop(element, target).perform()
+ 
