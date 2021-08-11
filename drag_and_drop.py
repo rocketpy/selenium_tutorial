22a1c30b8e365ff367ae7b664be1ddfa71f09694
rocketpy <dimitr.ushakov@gmail.com>
2021-08-11 23:37:45 +0300

    Update drag_and_drop.py

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -8,4 +8,17 @@ target = driver.find_element_by_name("target")
 from selenium.webdriver import ActionChains
 action_chains = ActionChains(driver)
 action_chains.drag_and_drop(element, target).perform()
+
+"""
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+driver = webdriver.Chrome()
+driver.get("http://")
+source_element = driver.find_element_by_id('bin')
+dest_element = driver.find_element_by_id('two')
+ActionChains(driver).drag_and_drop(source_element, dest_element).perform()
+
+"""
  
