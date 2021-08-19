293096f0ed2fd9512c0114c5d9fd299fc36b675a
rocketpy <dimitr.ushakov@gmail.com>
2021-08-19 23:26:28 +0300

    Update Webdriver-manager.py

--- Webdriver-manager.py
+++ Webdriver-manager.py
@@ -1,1 +1,30 @@
+# Library provides the way to automatically manage drivers for different browsers
+
+# PyPi: https://pypi.org/project/webdriver-manager/
+# Github: https://github.com/SergeyPirogov/webdriver_manager
+
+# pip install webdriver-manager
+
+# Before: You should download binary chromedriver, unzip it somewhere in you PC and set path to this driver like this:
+
+webdriver.Chrome('/home/user/drivers/chromedriver')
+
+ChromeDriverManager(path=custom_path).install()
+
+
+# Use with Chrome:
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# Use with Chromium:
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+
+
+driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+ 
 
