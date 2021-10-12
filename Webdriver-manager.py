9672782e28f0d26c59626c868a01b9b53b95f464
rocketpy <dimitr.ushakov@gmail.com>
2021-10-12 23:32:44 +0300

    Update Webdriver-manager.py

--- Webdriver-manager.py
+++ Webdriver-manager.py
@@ -16,21 +16,29 @@ ChromeDriverManager(path=custom_path).install()
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
 
 # Use with Chromium:
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 
-
 driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
 
 # Use with FireFox:
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+
+# Use with IE
+from selenium import webdriver
+from webdriver_manager.microsoft import IEDriverManager
+
+driver = webdriver.Ie(IEDriverManager().install())
+
  
 
