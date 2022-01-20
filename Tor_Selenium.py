598e2d2c6c1600fa8e74aefef428b7b11fc76651
rocketpy <dimitr.ushakov@gmail.com>
2022-01-20 23:35:16 +0200

    Update Tor_Selenium.py

--- Tor_Selenium.py
+++ Tor_Selenium.py
@@ -1,1 +1,20 @@
+#  tor-browser-selenium - A Python library to automate Tor Browser with Selenium.
+
+# Install geckodriver from:  https://github.com/mozilla/geckodriver/releases/
+
+# Github: https://github.com/webfp/tor-browser-selenium
+
+# pip install tbselenium
+
+
+# Usage
+
+# Using with system tor
+# tor needs to be installed (apt install tor) and running on port 9050.
+
+from tbselenium.tbdriver import TorBrowserDriver
+
+with TorBrowserDriver("/path/to/TorBrowserBundle/") as driver:
+    driver.get('https://check.torproject.org')
+
 
