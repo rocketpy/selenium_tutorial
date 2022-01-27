69eea6a7813ab51aba9028f0acfdc6eab65bfc0a
rocketpy <dimitr.ushakov@gmail.com>
2022-01-27 23:27:19 +0200

    Update working_with_table.py

--- working_with_table.py
+++ working_with_table.py
@@ -1,1 +1,11 @@
+import time
+import unittest
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+URL = "https://www.w3schools.com/html/html_tables.asp"
