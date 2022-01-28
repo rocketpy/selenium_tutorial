f785aeec2aa2fbbccf21f46f184d5b0ef584d90c
rocketpy <dimitr.ushakov@gmail.com>
2022-01-28 23:23:50 +0200

    Update working_with_table.py

--- working_with_table.py
+++ working_with_table.py
@@ -11,12 +11,20 @@ from selenium.webdriver.support import expected_conditions as EC
 URL = "https://www..."
 
 class TableDataTest(unittest.TestCase):
- 
+
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
- 
+
     def test_get_num_rows_(self):
         driver = self.driver
         driver.get(URL)
+       
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "")))
+    nums_rows = len(driver.find_elements_by_xpath("//*[@id='']/t/tr"))
+    
+    def test_get_num_cols_(self):
+        driver = self.driver
+        driver.get(test_url)
+
 
