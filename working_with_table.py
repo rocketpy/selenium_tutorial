616c738febfc4b518961b6b4108070f72cbfaef0
rocketpy <dimitr.ushakov@gmail.com>
2022-01-29 23:50:52 +0200

    Update working_with_table.py

--- working_with_table.py
+++ working_with_table.py
@@ -7,6 +7,14 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Important tags 
+"""
+ < table > – Defines an HTML table
+ < th > – Contains header information in a table
+ < tr > – Defines a row in a table
+ < td > – Defines a column in a table
+"""
+
 
 URL = "https://www..."
 
@@ -20,11 +28,15 @@ class TableDataTest(unittest.TestCase):
         driver = self.driver
         driver.get(URL)
        
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "")))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "")))
     nums_rows = len(driver.find_elements_by_xpath("//*[@id='']/t/tr"))
     
     def test_get_num_cols_(self):
         driver = self.driver
         driver.get(test_url)
+        
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "...")))
+        # nums_cols = len (driver.find_elements_by_xpath("//*[@id='']/tbody/tr/th"))
+        nums_cols = len (driver.find_elements_by_xpath("//*[@id='']/tbody/tr[2]/td"))
 
 
