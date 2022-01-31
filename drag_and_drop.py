6d6c4dab801d5c3adc53b7df86e4a367c9c34c43
rocketpy <dimitr.ushakov@gmail.com>
2022-01-31 23:01:06 +0200

    Update drag_and_drop.py

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -22,6 +22,7 @@ ActionChains(driver).drag_and_drop(source_element, dest_element).perform()
 
 """
 
+# Origin
 # Taked from: https://www.lambdatest.com/blog/drag-and-drop-in-selenium-python/
 """
 Performing Drag And Drop Using ActionChains Class
@@ -60,4 +61,26 @@ class DragTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         
+    def test_drag_drop(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get('https://jqueryui.com/draggable/') 
+        driver.switch_to.frame(0)
+        source1 = driver.find_element_by_id('draggable')
+        action = ActionChains(driver)
+        action.drag_and_drop_by_offset(source1, 100, 100).perform()
+        time.sleep(5)
+        driver.get('https://jqueryui.com/droppable/')
+        driver.switch_to.frame(0)
+        source1 = driver.find_element_by_id('draggable')
+        target1 = driver.find_element_by_id('droppable')
+        actions2 = ActionChains(driver)
+        actions2.drag_and_drop(source1, target1).perform()
+        time.sleep(5)
+        self.assertEqual("Dropped!", target1.text)
+        time.sleep(2)
+    def tearDown(self):
+        self.driver.quit()
  
+if __name__ == '__main__':
+    unittest.main()
