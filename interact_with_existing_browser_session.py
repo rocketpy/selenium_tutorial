381574afb368dabdf96385e28c8287cbb1e06558
rocketpy <dimitr.ushakov@gmail.com>
2022-04-28 23:56:19 +0300

    Update interact_with_existing_browser_session.py

--- interact_with_existing_browser_session.py
+++ interact_with_existing_browser_session.py
@@ -42,3 +42,18 @@ driver_1.get('http://www...')
 session_id = driver1.session_id
 """
 
+# or
+
+driver = webdriver.Firefox() 
+
+url = driver.command_executor._url
+session_id = driver.session_id
+
+driver = webdriver.Remote(command_executor=url,desired_capabilities={})
+driver.close()
+driver.session_id = session_id
+
+# connected to driver again.
+driver.get("http://www...")
+
+
