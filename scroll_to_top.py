2281adcbc8e09e3e6a4fdd8416448699ff98bf62
rocketpy <dimitr.ushakov@gmail.com>
2022-05-10 23:39:16 +0300

    Update scroll_to_top.py

--- scroll_to_top.py
+++ scroll_to_top.py
@@ -1,1 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# using send_keys
+html = driver.find_element_by_tag_name('html')
+html.send_keys(Keys.UP)
+
+# or
+
 driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
