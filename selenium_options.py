c341a18019362c8aac50dc3f7f81da5122d9674e
rocketpy <dimitr.ushakov@gmail.com>
2022-05-18 23:31:38 +0300

    Update selenium_options.py

--- selenium_options.py
+++ selenium_options.py
@@ -1,6 +1,15 @@
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 options = Options()
+
 options.add_argument("--headless")
 options.add_argument("--log-level NONE")
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+
