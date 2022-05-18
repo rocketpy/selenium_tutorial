833d8d131c21a7b8b4e90e364f12c35fd558b291
rocketpy <dimitr.ushakov@gmail.com>
2022-05-18 23:32:52 +0300

    Update selenium_options.py

--- selenium_options.py
+++ selenium_options.py
@@ -13,3 +13,13 @@ chrome_options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
+# for Firefox
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+firefox_options = Options()
+
+firefox_options.set_headless()
+driver = webdriver.Firefox(options=firefox_options)
+
