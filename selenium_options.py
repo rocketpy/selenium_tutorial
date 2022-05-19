306083873bafc0c436f980b941ba65c5d5e16971
rocketpy <dimitr.ushakov@gmail.com>
2022-05-19 23:43:45 +0300

    Update selenium_options.py

--- selenium_options.py
+++ selenium_options.py
@@ -23,3 +23,23 @@ firefox_options = Options()
 firefox_options.set_headless()
 driver = webdriver.Firefox(options=firefox_options)
 
+
+# Selenium Wire Options
+from seleniumwire import webdriver
+
+
+# selenium-wire proxy settings
+options = {'proxy': {'http': 'http://myusername:password@myproxyserver.com:123456', 
+                     'https': 'http://myusername:password@myproxyserver.com:123456', 
+                     'no_proxy': 'localhost,127.0.0.1'}}
+
+# other chrome options
+chrome_options = webdriver.ChromeOptions()
+
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-ssl-errors')
+chrome_options.add_argument('--ignore-certificate-errors-spki-list')
+
+driver = webdriver.Chrome('chromedriver', options=chrome_options, seleniumwire_options=options)
+
