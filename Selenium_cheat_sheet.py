729e759b350c95cd9d313a3bd4a78b7c6819fe51
rocketpy <dimitr.ushakov@gmail.com>
2022-09-09 23:59:37 +0300

    Update Selenium_cheat_sheet.py

--- Selenium_cheat_sheet.py
+++ Selenium_cheat_sheet.py
@@ -107,3 +107,6 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(chrome_options=chrome_options)
+
+chrome_options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options)
