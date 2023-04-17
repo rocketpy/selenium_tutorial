15c3d3305e3b6204f2f840a1527ac2bd61e0199a
rocketpy <dimitr.ushakov@gmail.com>
2023-04-17 23:32:34 +0300

    Update open_url_in_incognito.py

--- open_url_in_incognito.py
+++ open_url_in_incognito.py
@@ -3,3 +3,12 @@ import webbrowser
 url = 'www.blablabla.com'
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
 webbrowser.get(chrome_path).open_new(url)
+
+# or
+from selenium import webdriver
+
+c = webdriver.ChromeOptions()
+c.add_argument("--incognito")
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe",options=c)
+driver.implicitly_wait(0.5)
+driver.get("https://www.....com")
