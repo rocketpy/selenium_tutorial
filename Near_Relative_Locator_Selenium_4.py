b935346af6f55e3bedc8e5f87b1c3df3b8704611
rocketpy <dimitr.ushakov@gmail.com>
2023-04-21 23:41:51 +0300

    Update Near_Relative_Locator_Selenium_4.py

--- Near_Relative_Locator_Selenium_4.py
+++ Near_Relative_Locator_Selenium_4.py
@@ -1,1 +1,14 @@
+import sys
+import json
+import time
+import pytest
+import urllib3
+import warnings
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.relative_locator import with_tag_name
+
 
