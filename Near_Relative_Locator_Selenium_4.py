d8bb4cb5a0d48e8efccb6475eafca4c77a4418c4
rocketpy <dimitr.ushakov@gmail.com>
2023-04-22 23:56:28 +0300

    Update Near_Relative_Locator_Selenium_4.py

--- Near_Relative_Locator_Selenium_4.py
+++ Near_Relative_Locator_Selenium_4.py
@@ -12,3 +12,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.relative_locator import with_tag_name
 
 
+browser_capabilities = {
+        "user" : "user.name",
+        "accessKey" : "Access-Key",
+        "build" : "[Python] - Login Page",
+        "name" : "[Python] - Login Page",
+        "platformName" : "OS X Mavericks",
+        "browserName" : "Chrome",
+        "browserVersion" : "67.0"
+}
+user_name = "user-name"
+app_key = "access-key"
+
+def start_todo_app():
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    remote_url = "https://" + user_name + ":" + app_key + "@bm.com/wd/hub"
+    web_driver = webdriver.Remote(command_executor = remote_url, desired_capabilities = browser_capabilities)
+    web_driver.get('https://accounts.hey.com/login')
+    web_driver.maximize_window()
