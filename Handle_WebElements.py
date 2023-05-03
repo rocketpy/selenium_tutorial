3b653c75a1ea401659d420e5fec67bdc713529a0
rocketpy <dimitr.ushakov@gmail.com>
2023-05-03 23:53:53 +0300

    Update Handle_WebElements.py

--- Handle_WebElements.py
+++ Handle_WebElements.py
@@ -6,3 +6,11 @@ alert_popup = driver.switch_to.alert
 # The accept() method - accepts the alert message
 alert_popup = Alert(driver)
 alert_popup.accept()
+
+# The dismiss() method cancels the alert prompt
+alert_popup = Alert(driver)
+alert_popup.dismiss()
+
+# The text() method retrieves and displays the text that is in the alert pop-up
+alert_popup = Alert(driver)
+# print(alert_popup.text)
