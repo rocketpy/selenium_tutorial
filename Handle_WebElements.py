2f945f250b4c9fd32e5b45a117e8e069a3eedc37
rocketpy <dimitr.ushakov@gmail.com>
2023-05-04 23:54:01 +0300

    Update Handle_WebElements.py

--- Handle_WebElements.py
+++ Handle_WebElements.py
@@ -14,3 +14,18 @@ alert_popup.dismiss()
 # The text() method retrieves and displays the text that is in the alert pop-up
 alert_popup = Alert(driver)
 # print(alert_popup.text)
+
+# Drag and Drop Operations
+from selenium.webdriver import ActionChains
+
+action = ActionChains(driver)
+
+
+# Example
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options as ChromeOptions
