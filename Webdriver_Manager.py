ba236ac4abd9efc6c3d8b5392a4feafa7e2c0a6d
rocketpy <dimitr.ushakov@gmail.com>
2023-09-27 21:56:15 +0300

    Create Webdriver_Manager.py

---
+++ Webdriver_Manager.py
@@ -0,0 +1,5 @@
+# Webdriver Manager - Library provides the way to automatically manage drivers for different browsers
+
+# https://pypi.org/project/webdriver-manager/
+
+# pip install webdriver-manager
